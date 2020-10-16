30eb5e5b17f8a4478b62b324635a86431edba0dd
KASIM SINAN YILDIRIM <sinanyil81@gmail.com>
2020-10-16 09:35:45 +0200

    button example

---
+++ button/main.c
@@ -0,0 +1,25 @@
+#include "msp.h"
+
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	 P1->DIR = ~BIT1;   // Set P1.1 output
+	 P1->OUT = BIT1;    // Set pull-up mode
+	 P1->REN = BIT1;    // Enable pull-up resistor
+	 P1->SEL0 = 0;      // Select GPIO
+	 P1->SEL1 = 0;      // Select GPIO
+
+	 P1->DIR |= BIT0;   // P1.0 set as output
+
+	 while(1){
+	     while (P1->IN & BIT1);     // Check button pressed
+	     while (!(P1->IN & BIT1));  // Check button released
+
+	     P1->OUT ^= BIT0;   // Toggle P1.0 LED
+	 }
+}
